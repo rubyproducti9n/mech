fa4571ba1c93459d5ef27119d85f86499b7c1f59
rubyproducti9n <91490800+rubyproducti9n@users.noreply.github.com>
2022-05-15 00:05:25 +0530

    Add files via upload

--- user/om_73/range rover/js/global.js
+++ user/om_73/range rover/js/global.js
@@ -1,1 +1,46 @@
-var div;function home(){window.location.href="index.html"}function generations(){window.location.href="generations.html"}function expanded(){window.location.href="expanded.html"}function pricing(){window.location.href="pricing.html"}function models(){window.location.href="https://www.jaguar.com/jaguar-range/index.html"}function info(){window.location.href="info.html"}function jaguarInfo(){window.location.href="https://en.wikipedia.org/wiki/Land_Rover"}(div=document.getElementById("notice")).innerHTML='<center><img class="maintain-img" draggable="false" src="img/maintain.png"><br><div class="maintain-body">Opps! we are under maintainance, try again later.</div></center> ',(div=document.getElementById("navbar")).innerHTML='          <nav class="navbar">          \x3c!-- <div class="brand-name">              <a class="br-name">Group - 3</a>          </div> --\x3e          <ul>              <li class="nav-link" onclick="info()">                  <a class="link"> <i class="fa fa-info-circle nav-ic"></i>                    <span class="nav-strg">Info</span>                  </a>              </li>               <li class="nav-link" onclick="pricing()">               <a class="link"> <i class="fa fa-dollar-sign nav-ic"></i>                   <span class="nav-strg">Pricing</span>                 </a>           </li>               <li class="nav-link" onclick="expanded()">                  <a class="link"> <i class="fa fa-bus nav-ic"></i>                      <span class="nav-strg">Expanded</span>                    </a>              </li>              <li class="nav-link" onclick="home()">                  <a class="link"> <i class="fa fa-home nav-ic"></i>                      <span class="nav-strg">Home</span>                    </a>              </li>          </ul>        </nav> ',(div=document.getElementById("explore")).innerHTML='        <center>        <div class="ex-container">            <h3> <i class="fa fa-compass"></i> Explore</h3>            <div class="explore" onclick="home()">                Home            </div>            <div class="explore" onclick="generations()">                Car Generations            </div>            <div class="explore" onclick="pricing()">                Car Pricing            </div>            <div class="explore" onclick="pricing()">                Info            </div>        </div>        </center> ';var name="Om Lokhande | Akash Mhais | Prashant Londhe | Ruchika Mapari",roll="73, 74, 75, 76",branch="Mechanical",img="img/3.jpg";function stats(){var n=document.getElementById("stats");"-620px"==n.style.bottom?(n.style.bottom="20px",n.style.transition="0.5s ease"):(n.style.bottom="-620px",n.style.transition="0.5s ease")}document.getElementById("grp-img").innerHTML='<img src="'+img+'" class="grp-img" draggable="false">',document.getElementById("grp-nme").innerHTML=name,document.getElementById("grp-roll").innerHTML=roll,document.getElementById("grp-brh").innerHTML=branch;var globalImg=document.getElementsByTagName("img");globalImg.setAttribute("draggable","false");
+
+var div = document.getElementById("loading").innerHTML = ' <img id="loading-image" src="img/load.jpg" alt="Loading..." /> ';
+$(window).on('load', function () {
+    $('#loading').hide().delay(9000);
+  });
+
+function home() {
+    window.location.href = "index.html"
+}
+
+function generations() {
+    window.location.href = "generations.html"
+}
+
+function expanded() {
+    window.location.href = "expanded.html"
+}
+
+function pricing() {
+    window.location.href = "pricing.html"
+}
+
+function models() {
+    window.location.href = "https://www.jaguar.com/jaguar-range/index.html"
+}
+
+function info() {
+    window.location.href = "info.html"
+}
+
+function jaguarInfo() {
+    window.location.href = "https://en.wikipedia.org/wiki/Land_Rover"
+}(div = document.getElementById("notice")).innerHTML = '<center><img class="maintain-img" draggable="false" src="img/maintain.png"><br><div class="maintain-body">Oops! we are closed, try again later.</div></center> ', (div = document.getElementById("navbar")).innerHTML = '          <nav class="navbar">          \x3c!-- <div class="brand-name">              <a class="br-name">Group - 3</a>          </div> --\x3e          <ul>              <li class="nav-link" onclick="info()">                  <a class="link"> <i class="fa fa-info-circle nav-ic"></i>                    <span class="nav-strg">Info</span>                  </a>              </li>               <li class="nav-link" onclick="pricing()">               <a class="link"> <i class="fa fa-dollar-sign nav-ic"></i>                   <span class="nav-strg">Pricing</span>                 </a>           </li>               <li class="nav-link" onclick="expanded()">                  <a class="link"> <i class="fa fa-bus nav-ic"></i>                      <span class="nav-strg">Expanded</span>                    </a>              </li>              <li class="nav-link" onclick="home()">                  <a class="link"> <i class="fa fa-home nav-ic"></i>                      <span class="nav-strg">Home</span>                    </a>              </li>          </ul>        </nav> ', (div = document.getElementById("explore")).innerHTML = '        <center>        <div class="ex-container">            <h3> <i class="fa fa-compass"></i> Explore</h3>            <div class="explore" onclick="home()">                Home            </div>            <div class="explore" onclick="generations()">                Car Generations            </div>            <div class="explore" onclick="pricing()">                Car Pricing            </div>            <div class="explore" onclick="pricing()">                Info            </div>        </div>        </center> ';
+var name = "Om Lokhande | Akash Mhais | Prashant Londhe | Ruchika Mapari",
+    roll = "73, 74, 75, 76",
+    branch = "Mechanical",
+    img = "img/3.jpg";
+
+function stats() {
+    var n = document.getElementById("stats");
+    "-620px" == n.style.bottom ? (n.style.bottom = "20px", n.style.transition = "0.5s ease") : (n.style.bottom = "-620px", n.style.transition = "0.5s ease")
+}
+document.getElementById("grp-img").innerHTML = '<img src="' + img + '" class="grp-img" draggable="false">', document.getElementById("grp-nme").innerHTML = name, document.getElementById("grp-roll").innerHTML = roll, document.getElementById("grp-brh").innerHTML = branch;
+var globalImg = document.getElementsByTagName("img");
+globalImg.setAttribute("draggable", "false");
+
