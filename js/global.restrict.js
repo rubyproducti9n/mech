329708b32a6128c386eee58945398bdd885848e3
rubyproducti9n <91490800+rubyproducti9n@users.noreply.github.com>
2022-06-16 19:41:55 +0530

    Update global.restrict.js

--- js/global.restrict.js
+++ js/global.restrict.js
@@ -20,13 +20,13 @@ else
 return true;
 }
 });
-$(document).keydown(function (event) {
-    if (event.keyCode == 123) { // Prevent F12
-        return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
-        return false;
-    }
-});
+// $(document).keydown(function (event) {
+//     if (event.keyCode == 123) { // Prevent F12
+//         return false;
+//     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+//         return false;
+//     }
+// });
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 fetch('../vids.json')
