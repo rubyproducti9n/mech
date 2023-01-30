2418537fd5f2e6fdb4cb46735abf8041d0dc626f
rubyproducti9n <91490800+rubyproducti9n@users.noreply.github.com>
2023-01-30 22:23:50 +0530

    Add files via upload

--- js/id/6koSC.js
+++ js/id/6koSC.js
@@ -18,36 +18,79 @@ var v4detail = 'UNOFFICIAL MECH <i class="fa fa-check-circle"></i> • 08 Jul 20
 
 
 document.getElementById('6koSC').innerHTML = '\
-<div class="custom-mech-card">\
-    <h1>FEATURED</h1>\
-    <div class="mech-card " onclick="link1()">\
-    <img class="mech-card-img" draggable="false" src="'+ v1Thumbnail +'">\
-    <h3 class="dr-title placeholder"> '+ v1Title +' </h3>\
-    <h6 class="dr-details placeholder">\
-      <p class="">'+ v1detail +'</p>\
-    </h6>\
-  </div>\
-  <div class="mech-card " onclick="link1()">\
-  <img class="mech-card-img" draggable="false" src="'+ v2Thumbnail +'">\
-  <h3 class="dr-title placeholder"> '+ v2Title +' </h3>\
-  <h6 class="dr-details placeholder">\
-    <p class="">'+ v2detail +'</p>\
-  </h6>\
-</div>\
-<div class="mech-card " onclick="link1()">\
-<img class="mech-card-img" draggable="false" src="'+ v3Thumbnail +'">\
-<h3 class="dr-title placeholder"> '+ v3Title +' </h3>\
-<h6 class="dr-details placeholder">\
-  <p class="">'+ v3detail +'</p>\
-</h6>\
-</div>\
-<div class="mech-card " onclick="link1()">\
-<img class="mech-card-img" draggable="false" src="'+ v4Thumbnail +'">\
-<h3 class="dr-title placeholder"> '+ v4Title +' </h3>\
-<h6 class="dr-details placeholder">\
-  <p class=""> '+ v4detail +' </p>\
-</h6>\
-</div>\
+<div class="ind-cards-container">\
+\
+<div class="ind-card" onclick="link1()">\
+    <img class="ind-img" src="'+ v1Thumbnail +'">\
+    <img class="channel-img" src="img/avatar.jpg">\
+    <div class="ind-details">\
+        <p class="ind-title"> '+ v1Title +' </p>\
+        <p class="ind-other-detail"> '+ v1detail +' </p>\
+    </div>\
 </div>\
 \
+<div class="ind-card" onclick="link2()">\
+    <img class="ind-img" src="'+ v2Thumbnail +'">\
+    <img class="channel-img" src="img/avatar.jpg">\
+    <div class="ind-details">\
+        <p class="ind-title"> '+ v2Title +' </p>\
+        <p class="ind-other-detail"> '+ v2detail +' </p>\
+    </div>\
+</div>\
+\
+<div class="ind-card" onclick="link3()">\
+    <img class="ind-img" src="'+ v3Thumbnail +'">\
+    <img class="channel-img" src="img/avatar.jpg">\
+    <div class="ind-details">\
+        <p class="ind-title"> '+ v3Title +' </p>\
+        <p class="ind-other-detail"> '+ v3detail +' </p>\
+    </div>\
+</div>\
+\
+<div class="ind-card" onclick="link4()">\
+    <img class="ind-img" src="'+ v4Thumbnail +'">\
+    <img class="channel-img" src="img/avatar.jpg">\
+    <div class="ind-details">\
+        <p class="ind-title"> '+ v4Title +' </p>\
+        <p class="ind-other-detail"> '+ v4detail +' </p>\
+    </div>\
+</div>\
+\
+</div>\
 ';
+
+
+// document.getElementById('6koSC').innerHTML = '\
+// <div class="custom-mech-card">\
+//     <h1>FEATURED</h1>\
+//     <div class="mech-card " onclick="link1()">\
+//     <img class="mech-card-img" draggable="false" src="'+ v1Thumbnail +'">\
+//     <h3 class="dr-title placeholder"> '+ v1Title +' </h3>\
+//     <h6 class="dr-details placeholder">\
+//       <p class="">'+ v1detail +'</p>\
+//     </h6>\
+//   </div>\
+//   <div class="mech-card " onclick="link1()">\
+//   <img class="mech-card-img" draggable="false" src="'+ v2Thumbnail +'">\
+//   <h3 class="dr-title placeholder"> '+ v2Title +' </h3>\
+//   <h6 class="dr-details placeholder">\
+//     <p class="">'+ v2detail +'</p>\
+//   </h6>\
+// </div>\
+// <div class="mech-card " onclick="link1()">\
+// <img class="mech-card-img" draggable="false" src="'+ v3Thumbnail +'">\
+// <h3 class="dr-title placeholder"> '+ v3Title +' </h3>\
+// <h6 class="dr-details placeholder">\
+//   <p class="">'+ v3detail +'</p>\
+// </h6>\
+// </div>\
+// <div class="mech-card " onclick="link1()">\
+// <img class="mech-card-img" draggable="false" src="'+ v4Thumbnail +'">\
+// <h3 class="dr-title placeholder"> '+ v4Title +' </h3>\
+// <h6 class="dr-details placeholder">\
+//   <p class=""> '+ v4detail +' </p>\
+// </h6>\
+// </div>\
+// </div>\
+// \
+// ';
