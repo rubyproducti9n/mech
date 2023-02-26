0c8cc863f791deb843dc0b8e233a6634de94c33c
rubyproducti9n <91490800+rubyproducti9n@users.noreply.github.com>
2023-02-26 23:25:46 +0530

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -1,6 +1,15 @@
 
   var img = document.getElementById('c1');
   img.innerHTML = '\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img1.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img2.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img3.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img4.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img5.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img6.jpg" style="width:100%">\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/img1.jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/m (12).jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/g12.jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/m (2).jpg" style="width:100%">\
@@ -13,6 +22,13 @@
   ';
   var img = document.getElementById('c2');
   img.innerHTML = '\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img7.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img8.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img9.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img10.jpg" style="width:100%">\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/img2.jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/m (9).jpg" style="width:100%">\
     <img draggable="false" oncontextmenu="return false;" src="memories/m (1).jpg" style="width:100%">\
     <img draggable="false" oncontextmenu="return false;" src="memories/g5.jpg" style="width:100%">\
@@ -26,6 +42,13 @@
     ';
   var img = document.getElementById('c3');
   img.innerHTML = '\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img11.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img12.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img13.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img14.jpg" style="width:100%">\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/img3.jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/m (13).jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/m (10).jpg" style="width:100%">\
     <img draggable="false" oncontextmenu="return false;" src="memories/g3.jpg" style="width:100%">\
@@ -40,6 +63,15 @@
     ';
   var img = document.getElementById('c4');
   img.innerHTML = '\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img15.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img16.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img17.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img18.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img19.jpg" style="width:100%">\
+  <img draggable="false" oncontextmenu="return false;" src="memories/manmad workshop/img20.jpg" style="width:100%">\
+  \
+  <img draggable="false" oncontextmenu="return false;" src="memories/img4.jpg" style="width:100%">\
   <img draggable="false" oncontextmenu="return false;" src="memories/m (11).jpg" style="width:100%">\
     <img draggable="false" oncontextmenu="return false;" src="memories/g13.jpg" style="width:100%">\
       <img draggable="false" oncontextmenu="return false;" src="memories/m (1).jpg" style="width:100%">\
